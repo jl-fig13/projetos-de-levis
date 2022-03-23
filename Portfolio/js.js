2e86b32cbddda520f404cb0f667c24facae259b3
J. Lucas Figueiredo <jlucasfigueiredo@hotmail.com.br>
2022-03-23 14:24:04 -0300

    Site de Portfolio
    Site que elaborei utilizando a linguagem html/css e js para apresentar meu portfolio.

---
+++ Portfolio/js.js
@@ -0,0 +1,12 @@
+// MODAL
+    function showModal(){
+        var element = document.getElementById("modal");
+        element.classList.add("show-modal");
+    }
+
+    function hidenModal(){
+        var element = document.getElementById("modal");
+        element.classList.remove("show-modal"); 
+    }
+
+//
